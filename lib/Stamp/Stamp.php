8f0a2909a0643d1a9c97f2bc4b0df0a297ee6c6d
sean <sean.oreilly@epicdigitalmedia.com>
2013-02-14 09:49:38 +0000

    updates to psr2 standard

--- lib/Stamp/Stamp.php
+++ lib/Stamp/Stamp.php
@@ -4,42 +4,49 @@ namespace Stamp;
 
 class Stamp
 {
-  private $cache = array();
+    /**
+     * @var array
+     */
+    private $cache = array();
 
-  public function stamp($example, $time)
-  {
-
-    $translator = $this->fetchTranslator( $example );
-
-    // what is our argument?
-    if ( !is_numeric($time) )
+    /**
+     * @param $example
+     * @param $time
+     * @return string
+     * @throws \Exception
+     */
+    public function stamp($example, $time)
     {
-      if ( is_string($time) )
-      {
-        $time = strtotime($time);
-      }
-      elseif( is_object( $time ) && get_class($time) == 'DateTime')
-      {
-        $time = $time->getTimestamp();
-      }
-      else
-      {
-        throw new Exception('Invalid date');
-      }
+
+        $translator = $this->fetchTranslator($example);
+
+        // what is our argument?
+        if (!is_numeric($time)) {
+            if (is_string($time)) {
+                $time = strtotime($time);
+            } elseif (is_object($time) && get_class($time) == 'DateTime') {
+                $time = $time->getTimestamp();
+            } else {
+                throw new \Exception('Invalid date');
+            }
+        }
+
+
+        return $translator->translate($time);
     }
 
+    /**
+     * @param $example
+     * @return Translator
+     */
+    private function fetchTranslator($example)
+    {
+        if (isset($this->cache[$example])) {
+            return $this->cache[$example];
+        }
 
-
-    return $translator->translate($time);
-  }
-
-  private function fetchTranslator($example) {
-    if ( isset( $this->cache[$example] ) ) {
-      return $this->cache[$example];
+        $translator = new Translator($example);
+        $this->cache[$example] = $translator;
+        return $translator;
     }
-
-    $translator = new Translator( $example );
-    $this->cache[$example] = $translator;
-    return $translator;
-  }
 }
