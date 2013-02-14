1984d1d7060382e01752e089b178bee38b81517c
sean <sean.oreilly@epicdigitalmedia.com>
2013-02-14 11:03:51 +0000

    made cache static, made time param optional defaults to current time

--- lib/Stamp/Stamp.php
+++ lib/Stamp/Stamp.php
@@ -7,7 +7,7 @@ class Stamp
     /**
      * @var array
      */
-    private $cache = array();
+    private static $cache = array();
 
     /**
      * @param $example
@@ -15,22 +15,24 @@ class Stamp
      * @return string
      * @throws \Exception
      */
-    public function stamp($example, $time)
+    public function stamp($example, $time = null)
     {
-
         $translator = $this->fetchTranslator($example);
 
-        // what is our argument?
-        if (!is_numeric($time)) {
-            if (is_string($time)) {
-                $time = strtotime($time);
-            } elseif (is_object($time) && get_class($time) == 'DateTime') {
-                $time = $time->getTimestamp();
-            } else {
-                throw new \Exception('Invalid date');
+        if (is_null($time)) {
+            $time = time();
+        } else {
+            // what is our argument?
+            if (!is_numeric($time)) {
+                if (is_string($time)) {
+                    $time = strtotime($time);
+                } elseif ($time instanceof \DateTime) {
+                    $time = $time->getTimestamp();
+                } else {
+                    throw new \Exception('Invalid Date');
+                }
             }
         }
-
 
         return $translator->translate($time);
     }
@@ -41,12 +43,12 @@ class Stamp
      */
     private function fetchTranslator($example)
     {
-        if (isset($this->cache[$example])) {
-            return $this->cache[$example];
+        if (isset(self::$cache[$example])) {
+            return self::$cache[$example];
         }
 
         $translator = new Translator($example);
-        $this->cache[$example] = $translator;
+        self::$cache[$example] = $translator;
         return $translator;
     }
 }
